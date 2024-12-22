2e33228c828365fd36f591097783774682078988
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2024-12-22 13:23:21 +0100

    typesafeConventionsInternalCatalog

--- src/main/kotlin/dev/panuszewski/gradle/TypesafeConventionsPlugin.kt
+++ src/main/kotlin/dev/panuszewski/gradle/TypesafeConventionsPlugin.kt
@@ -2,18 +2,15 @@ package dev.panuszewski.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.internal.GradleInternal
 import org.gradle.api.internal.catalog.LibrariesSourceGenerator
 import org.gradle.api.internal.catalog.parser.TomlCatalogFileParser
-import org.gradle.api.internal.project.DefaultProject
-import org.gradle.api.plugins.catalog.CatalogPluginExtension
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.internal.management.VersionCatalogBuilderInternal
-import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.support.serviceOf
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetContainer
 import java.io.StringWriter
-import java.nio.file.Paths
 
 class TypesafeConventionsPlugin : Plugin<Project> {
 
@@ -22,16 +19,20 @@ class TypesafeConventionsPlugin : Plugin<Project> {
     }
 
     private fun Project.configure() {
-        apply(plugin = "version-catalog")
+        val versionCatalogs = (gradle as GradleInternal).settings
+            .dependencyResolutionManagement
+            .versionCatalogs
 
-        var versionCatalogBuilder: VersionCatalogBuilderInternal? = null
-        configure<CatalogPluginExtension> {
-            versionCatalog {
-                versionCatalogBuilder = this as VersionCatalogBuilderInternal
+        val versionCatalogName = "typesafeConventionsInternalCatalog"
+
+        val versionCatalogBuilder = versionCatalogs.findByName(versionCatalogName)
+            ?: run {
+                val builder = versionCatalogs.create(versionCatalogName)
+                TomlCatalogFileParser.parse(rootProject.file("../gradle/libs.versions.toml").toPath(), builder, serviceOf())
+                builder
             }
-        }
-        TomlCatalogFileParser.parse(rootProject.file("../gradle/libs.versions.toml").toPath(), versionCatalogBuilder, serviceOf())
-        val model = versionCatalogBuilder?.build()!!
+
+        val model = (versionCatalogBuilder as VersionCatalogBuilderInternal).build()
 
         val file = file("build/generated/typesafe/main/java/org/gradle/accessors/dm/LibrariesForLibs.java")
         file.parentFile.mkdirs()
@@ -46,35 +47,28 @@ class TypesafeConventionsPlugin : Plugin<Project> {
         libsFile.writeText("""
             import org.gradle.accessors.dm.LibrariesForLibs
             import org.gradle.api.Project
-            import org.gradle.api.plugins.catalog.CatalogPluginExtension
+            import org.gradle.api.internal.GradleInternal
             import org.gradle.api.internal.catalog.parser.TomlCatalogFileParser
-            import org.gradle.api.internal.project.ProjectInternal
-            import org.gradle.internal.extensions.core.get
             import org.gradle.internal.management.VersionCatalogBuilderInternal
-            import org.gradle.kotlin.dsl.configure
             import org.gradle.kotlin.dsl.newInstance
-
+            import org.gradle.kotlin.dsl.support.serviceOf
+            
             internal val Project.libs: LibrariesForLibs
                 get() {
-                    var versionCatalogBuilder: VersionCatalogBuilderInternal? = null
-
-                    if (!plugins.hasPlugin("version-catalog")) {
-                        apply(mapOf("plugin" to "version-catalog"))
-
-                        configure<CatalogPluginExtension> {
-                            versionCatalog {
-                                versionCatalogBuilder = this as VersionCatalogBuilderInternal
-                            }
+                    val versionCatalogs = (gradle as GradleInternal).settings
+                        .dependencyResolutionManagement
+                        .versionCatalogs
+                    
+                    val versionCatalogName = "typesafeConventionsInternalCatalog"
+                    
+                    val versionCatalogBuilder = versionCatalogs.findByName(versionCatalogName)
+                        ?: run {
+                            val builder = versionCatalogs.create(versionCatalogName)
+                            TomlCatalogFileParser.parse(rootProject.file("gradle/libs.versions.toml").toPath(), builder, serviceOf())
+                            builder
                         }
-                        TomlCatalogFileParser.parse(rootProject.file("gradle/libs.versions.toml").toPath(), versionCatalogBuilder, (project as ProjectInternal).services.get())
-                    } else {
-                        configure<CatalogPluginExtension> {
-                            versionCatalog {
-                                versionCatalogBuilder = this as VersionCatalogBuilderInternal
-                            }
-                        }
-                    }
-                    val model = versionCatalogBuilder?.build()!!
+            
+                    val model = (versionCatalogBuilder as VersionCatalogBuilderInternal).build()
                     return objects.newInstance(LibrariesForLibs::class, model)
                 }
         """.trimIndent())
