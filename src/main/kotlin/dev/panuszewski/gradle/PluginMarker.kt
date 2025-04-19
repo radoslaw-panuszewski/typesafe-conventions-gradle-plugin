d93aa7cdcb60b50866546cbbcf0b7012a746d919
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2025-04-19 15:51:36 +0200

    Support rich versions in pluginMarker function

--- src/main/kotlin/dev/panuszewski/gradle/PluginMarker.kt
+++ src/main/kotlin/dev/panuszewski/gradle/PluginMarker.kt
@@ -1,9 +1,26 @@
 package dev.panuszewski.gradle
 
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.VersionConstraint
 import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.create
 import org.gradle.plugin.use.PluginDependency
 
-public fun pluginMarker(provider: Provider<PluginDependency>): String {
+public fun Project.pluginMarker(provider: Provider<PluginDependency>): Dependency {
     val plugin = provider.get()
-    return "${plugin.pluginId}:${plugin.pluginId}.gradle.plugin:${plugin.version}"
+    return dependencyWithRichVersion(
+        group = plugin.pluginId,
+        name = "${plugin.pluginId}.gradle.plugin",
+        versionConstraint = plugin.version
+    )
 }
+
+private fun Project.dependencyWithRichVersion(group: String, name: String, versionConstraint: VersionConstraint) =
+    dependencies.create("$group:$name") {
+        version {
+            versionConstraint.strictVersion.takeIf(String::isNotBlank)?.let(::strictly)
+            versionConstraint.requiredVersion.takeIf(String::isNotBlank)?.let(::require)
+            versionConstraint.preferredVersion.takeIf(String::isNotBlank)?.let(::prefer)
+        }
+    }
